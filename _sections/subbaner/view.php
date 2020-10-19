a4c2ce8d8d18bc1511a142967d490caf51aefcc9
G3ck Dev <dev@g3ck.com>
2020-10-19 04:56:57 +0200

    init

--- _sections/subbaner/view.php
+++
@@ -1,4 +0,0 @@
-<section id="clSection_<?php echo $clSection['id']; ?>" class="clSection subbaner" style="background-image:url('<?php echo $clSection['url']; ?>background.jpg');">
-<div class="layer"></div>
-</div>
-</section>
--- _sections/text-caption/view.php
+++
@@ -1,9 +0,0 @@
-<section id="clSection_<?php echo $clSection['id']; ?>" class="clSection text-caption <?php echo @$clSection['meta']['class']; ?>">
-    <div class="container">
-        <div class="line"><div class="inner"></div></div>
-        <h1><?php echo $clSection['meta']['header']; ?></h1>
-        <?php if (@$clSection['meta']['subheader'] != ''): ?>
-        <h4><?php echo $clSection['meta']['subheader']; ?></h4>
-        <?php endif; ?>
-    </div>
-</section>
